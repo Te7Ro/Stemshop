087fc04eec9d83791878caab19846ae518678768
Otegen <otegen3439@gmail.com>
2025-08-11 14:04:28 +0500

    first commit

---
+++ src/main/java/com/example/stemshop/StemshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.stemshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StemshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StemshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stemshop/StemshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stemshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StemshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
